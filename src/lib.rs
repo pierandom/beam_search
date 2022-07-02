3a4935e05b9ea2e15b598713d350d5dd364ef182
Pierandrea Sartori <pierandrea.sartori@gmail.com>
2022-07-02 17:10:49 +0200

    💄 Add default parameters in python

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,41 @@ use pyo3::{prelude::*, types::PySequence};
 
 mod core;
 
-#[pyfunction]
+#[pyfunction(beam_width="10", topk_paths="1", charset="None")]
 fn decode(
     py: Python,
     probs: PyReadonlyArray2<f32>,
     alphabet: &PySequence,
     beam_width: usize,
     topk_paths: usize,
-    charset: &PySequence,
+    charset: Option<&PySequence>,
 ) -> PyResult<Vec<(String, f32)>> {
     let probs = probs.as_array();
     let alphabet: Vec<String> = alphabet.tuple()?.iter().map(|x| x.to_string()).collect();
-    let charset: Vec<String> = charset.tuple()?.iter().map(|x| x.to_string()).collect();
+    let charset: Vec<String> = match charset {
+        Some(cs) => cs.tuple()?.iter().map(|x| x.to_string()).collect(),
+        None => Vec::new()
+    };
     let predictions =
         py.allow_threads(move || core::decode(probs, &alphabet, beam_width, topk_paths, &charset));
     Ok(predictions)
 }
 
-#[pyfunction]
+#[pyfunction(beam_width="10", topk_paths="1", charset="None")]
 fn decode_batch(
     py: Python,
     probs: PyReadonlyArray3<f32>,
     alphabet: &PySequence,
     beam_width: usize,
     topk_paths: usize,
-    charset: &PySequence,
+    charset: Option<&PySequence>,
 ) -> PyResult<Vec<Vec<(String, f32)>>> {
     let probs = probs.as_array();
     let alphabet: Vec<String> = alphabet.tuple()?.iter().map(|x| x.to_string()).collect();
-    let charset: Vec<String> = charset.tuple()?.iter().map(|x| x.to_string()).collect();
+    let charset: Vec<String> = match charset {
+        Some(cs) => cs.tuple()?.iter().map(|x| x.to_string()).collect(),
+        None => Vec::new()
+    };
     let predictions = py.allow_threads(move || {
         core::decode_batch(probs, &alphabet, beam_width, topk_paths, &charset)
     });
